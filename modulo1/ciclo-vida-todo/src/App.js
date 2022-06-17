d567a3a32d4770f10cb9ae7ba5880bed7ec356e8
“Bruno <bruno.feitosa@grupoboticario.com.br>
2022-06-17 10:21:19 -0300

    Adicionando função de deletar e começando a de editar tarefas

--- modulo1/ciclo-vida-todo/src/App.js
+++ modulo1/ciclo-vida-todo/src/App.js
@@ -1,15 +1,45 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import "./styles.css";
+import deleteButton from "../src/img/deleteButton.png";
 
 const TarefaList = styled.ul`
   padding: 0;
-  width: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  list-style-type: none;
+`;
+
+const TarefaAndInputDiv = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ChangeValueDiv = styled.div`
+  display: ${({ displayChangeInput }) =>
+    displayChangeInput ? "none" : "grid"};
+  grid-template-columns: 3fr 1fr;
+  margin-left: 20px;
+  position: absolute;
+  left: 600px;
+`;
+
+const ChangeValueInput = styled.input`
+  margin-right: 2rem;
+`;
+
+const ChangeValueButton = styled.button``;
+
+const DeleteButtonImage = styled.img`
+  margin-right: 0.5rem;
+  width: 20px;
 `;
 
 const Tarefa = styled.li`
   text-align: left;
   text-decoration: ${({ completa }) => (completa ? "line-through" : "none")};
+  margin: 10px;
 `;
 
 const InputsContainer = styled.div`
@@ -25,10 +55,13 @@ function App() {
   ]);
   const [inputValue, setInputValue] = useState("");
   const [filtro, setFiltro] = useState("filtradas");
+  const [changeValueState, setChangeValueState] = useState("");
+  const [displayChangeInput, setDisplayChangeInput] = useState(false);
+  // const [checkboxState, setCheckboxState] = useState(true);
 
-  const saveData = () => {
-    if (inputValue) localStorage.setItem("inputValue", inputValue);
-  };
+  // const saveData = () => {
+  //   if (inputValue) localStorage.setItem("inputValue", inputValue);
+  // };
 
   useEffect(() => {
     const storedInput = localStorage.getItem("inputValue");
@@ -36,12 +69,24 @@ function App() {
   }, []);
 
   useEffect(() => {
-    saveData();
+    if (inputValue === "") localStorage.setItem("inputValue", "");
+    else {
+      localStorage.setItem("inputValue", inputValue);
+    }
   }, [inputValue]);
 
   const onChangeInput = (event) => {
     setInputValue(event.target.value);
   };
+
+  const handleChangeValue = (event) => {
+    setChangeValueState(event.target.value);
+  };
+
+  // const markCheckbox = () => {
+  //   setCheckboxState(!checkboxState);
+  //   console.log(checkboxState);
+  // };
 
   const criaTarefa = () => {
     const novaTarefa = {
@@ -106,14 +151,49 @@ function App() {
       </InputsContainer>
       <TarefaList>
         {listaFiltrada.map((tarefa) => {
+          const apagaTarefa = () => {
+            if (window.confirm("Deseja apagar a tarefa selecionada?")) {
+              const novaListaDeTarefas = [...listaFiltrada];
+              const indice = novaListaDeTarefas.findIndex((index) => {
+                return index === tarefa;
+              });
+              novaListaDeTarefas.splice(indice, 1);
+              setTarefa(novaListaDeTarefas);
+            }
+          };
+
+          const editaTarefas = () => {
+            const novaListaDeTarefas = [...listaFiltrada];
+            const indice = novaListaDeTarefas.findIndex((index) => {
+              return index === tarefa;
+            });
+            console.log(indice);
+
+            // novaListaDeTarefas.splice(indice, 1, changeValueState);
+            // setTarefa(novaListaDeTarefas);
+          };
           return (
-            <Tarefa
-              key={tarefa.id}
-              completa={tarefa.completa}
-              onClick={() => selectTarefa(tarefa.id)}
-            >
-              {tarefa.texto}
-            </Tarefa>
+            <TarefaAndInputDiv key={tarefa.id}>
+              <DeleteButtonImage
+                src={deleteButton}
+                onClick={apagaTarefa}
+              ></DeleteButtonImage>
+              <Tarefa
+                completa={tarefa.completa}
+                onClick={() => selectTarefa(tarefa.id)}
+              >
+                {tarefa.texto}
+              </Tarefa>
+              <ChangeValueDiv>
+                <ChangeValueInput
+                  onChange={handleChangeValue}
+                  value={changeValueState}
+                ></ChangeValueInput>
+                <ChangeValueButton onClick={editaTarefas}>
+                  Atualizar
+                </ChangeValueButton>
+              </ChangeValueDiv>
+            </TarefaAndInputDiv>
           );
         })}
       </TarefaList>

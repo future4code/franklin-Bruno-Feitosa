3be8c94b1c617b995f111fc89f2eabd79572a853
“Bruno <bruno.feitosa@grupoboticario.com.br>
2022-06-27 21:32:03 -0300

    Add Track Details

--- modulo1/projeto-labefy/src/App.js
+++ modulo1/projeto-labefy/src/App.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import TrackDetails from "./components/TrackDetails/TrackDetails";
+
+const AddMusicDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+`;
 
 function App() {
   const [playlists, setPlaylistsState] = useState([]);
@@ -8,6 +16,7 @@ function App() {
   const [inputMusicName, setInputMusicName] = useState("");
   const [inputArtist, setInputArtist] = useState("");
   const [inputMusicUrl, setInputMusicUrl] = useState("");
+  const [trackDetail, setTrackDetail] = useState([]);
 
   const handleInputPlaylist = (e) => {
     setInputPlaylist(e.target.value);
@@ -93,13 +102,69 @@ function App() {
       });
   };
 
+  const getPlaylistTracks = (playlistId) => {
+    axios
+      .get(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${playlistId}/tracks`,
+        {
+          headers: {
+            Authorization: "bruno-feitosa-franklin",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        setTrackDetail(response.data.result.tracks);
+        // alert("Exibindo detalhes da playlist");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const deletePlaylist = (playlistId) => {
+    axios
+      .delete(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${playlistId}`,
+        {
+          headers: {
+            Authorization: "bruno-feitosa-franklin",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  const displayTrackDetails = () => {
+    return <TrackDetails />;
+  };
+
   return (
     <div>
-      <div>
-        <input value={inputMusicName} onChange={handleInputMusicName}></input>
-        <input value={inputArtist} onChange={handleInputArtist}></input>
-        <input value={inputMusicUrl} onChange={handleInputMusicUrl}></input>
-      </div>
+      <AddMusicDiv>
+        <label>Adicionar Música</label>
+        <input
+          value={inputMusicName}
+          placeholder="Nome da música"
+          onChange={handleInputMusicName}
+        ></input>
+        <input
+          value={inputArtist}
+          placeholder="Nome do artista"
+          onChange={handleInputArtist}
+        ></input>
+        <input
+          value={inputMusicUrl}
+          placeholder="URL da música"
+          onChange={handleInputMusicUrl}
+        ></input>
+      </AddMusicDiv>
       <input value={inputPlaylist} onChange={handleInputPlaylist}></input>
       <button onClick={createPlaylist}>Criar Playlist</button>
 
@@ -117,6 +182,14 @@ function App() {
             >
               Adicionar Track
             </button>
+            <button
+              onClick={() => {
+                deletePlaylist(playlist.id);
+              }}
+            >
+              Apagar Playlist
+            </button>
+            <button>Detalhes da Playlist</button>
           </div>
         );
       })}

12eef5d7d2a641e86be5b94564621ac2f445bc8e
“Bruno <bruno.feitosa@grupoboticario.com.br>
2022-07-20 17:43:27 -0300

    Corrigindo arquivos do projeto

--- modulo2/7bugs-teste/src/App.test.js
+++ modulo2/7bugs-teste/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
+test("Teste 1", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  userEvent.click(
+    screen.getByRole("button", {
+      name: /carrinho/i
+    })
+  );
+});
+
+test("Teste 2", () => {
+  render(<App />);
+
+  userEvent.click(
+    screen.getByRole("button", {
+      name: /carrinho/i
+    })
+  );
 });

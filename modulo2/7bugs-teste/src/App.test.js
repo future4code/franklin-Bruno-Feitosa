ae521e444e9384db593384864a194c206bf86ae7
“Bruno <bruno.feitosa@grupoboticario.com.br>
2022-07-26 14:49:53 -0300

    Adicionando teste de valor total

--- modulo2/7bugs-teste/src/App.test.js
+++ modulo2/7bugs-teste/src/App.test.js
@@ -1,24 +1,42 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 import userEvent from "@testing-library/user-event";
+import ProductCard from "./components/ProductCard/ProductCard";
+import CartScreen from "./screens/CartScreen/CartScreen";
 
-test("Teste 1", () => {
-  render(<App />);
+test("Comprar um produto", () => {
+  const { getByText } = render(<ProductCard />);
+
+  userEvent.click(getByText(/comprar/i));
+  userEvent.click(getByText(/comprar/i));
+});
+
+test("Ir para o carrinho", () => {
+  const { getByRole } = render(<App />);
 
   userEvent.click(
-    screen.getByRole("button", {
-      name: /carrinho/i
+    getByRole("button", {
+      name: /carrinho/i,
     })
   );
 });
 
-test("Teste 2", () => {
-  render(<App />);
+// test("Remover item do carrinho", () => {
+//   const { getByRole } = render(<ProductItem />);
 
-  userEvent.click(
-    screen.getByRole("button", {
-      name: /carrinho/i
-    })
-  );
+//   userEvent.click(
+//     getByRole("button", {
+//       name: /remover/i,
+//     })
+//   );
+// });
+
+test("Verifica se o total do carrinho vazio é 0", () => {
+  const { getByText } = render(<App />);
+
+  const totalValue = getByText(/Total/i);
+  // console.log(getByText(/Total/i));
+  // console.log(total);
+  expect(totalValue.textContent).toBe("Total: R$0");
 });

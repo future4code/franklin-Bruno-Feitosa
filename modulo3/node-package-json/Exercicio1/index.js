3b17cfdbafdd2b331f9aa8d4ee1dd35a4fd3cacb
“Bruno <bruno.feitosa@grupoboticario.com.br>
2022-08-04 00:12:08 -0300

    Concluindo os desafios

--- modulo3/node-package-json/Exercicio1/index.js
+++ modulo3/node-package-json/Exercicio1/index.js
@@ -1,9 +1,28 @@
 // Exercício 1
 // a) Os argumentos passados na linha de comando para o Node podem ser acessados utilizando o process.argv[2]. Sendo o [0] o próprio caminho do node, o [1] o caminho do arquivo executado (index.js) e o [2] o primeiro argumento passado pela linha de comando.
-
 // b) Para rodar, pode usar o script nomeIdade.
-console.log(
-  `Olá, ${process.argv[2]}! Você tem ${
-    process.argv[3]
-  } anos. Em sete anos você terá ${Number(process.argv[4]) + 7}`
-);
+
+// Desafio: Verificar se estamos passando os 2 parâmetros. Já que não podemos acessar a terceira posição do process.argv sem passar a segunda, faremos a seguinte verificação:
+
+// Cores -> Coloquei duas formas de passar. A primeira concatenando com o operador + e a segunda dentro de um template string.
+const magenta = "\x1b[35m";
+const red = "\u001b[31m";
+
+if (!process.argv[2]) console.log(red + "Você precisa passar dois parâmetros");
+else if (process.argv[2] && !process.argv[3])
+  console.log(red + "Você passou um parâmetro, precisamos de mais um!");
+else {
+  Number(process.argv[3]) === 1
+    ? console.log(
+        `${magenta} Olá, ${process.argv[2]}! Você tem ${
+          process.argv[3]
+        } ano. Daqui à sete anos você terá ${Number(process.argv[3]) + 7} anos!`
+      )
+    : console.log(
+        `${magenta} Olá, ${process.argv[2]}! Você tem ${
+          process.argv[3]
+        } anos. Daqui à sete anos você terá ${
+          Number(process.argv[3]) + 7
+        } anos!`
+      );
+}
--- modulo3/node-package-json/Exercicio3/index.js
+++ modulo3/node-package-json/Exercicio3/index.js
@@ -1,12 +1,26 @@
 // Exercício 3 - Olhar o script listaDeTarefas
 // Para rodar temos dois parâmetros. O primeiro (posição 2 do process.argv) é o número de tarefas que queremos cadastrar. Os outros parâmetros são as tarefas que colocaremos na lista.
 
+//Cores
+const red = "\u001b[31m";
+
 let listaDeTarefas = [];
 
 const numeroDeTarefas = Number(process.argv[2]) + 3;
 
-for (let i = 3; i < numeroDeTarefas; i++) {
-  let novaTarefa = process.argv[i];
-  novaTarefa === undefined ? null : listaDeTarefas.push(novaTarefa);
+if (!process.argv[2])
+  console.log(
+    red + "Você precisa passar o número de tarefas que serão adicionadas!"
+  );
+else {
+  for (let i = 3; i < numeroDeTarefas; i++) {
+    let novaTarefa = process.argv[i];
+    novaTarefa === undefined ? null : listaDeTarefas.push(novaTarefa);
+  }
+  listaDeTarefas.length === 0
+    ? console.log(
+        red +
+          "Você precisa passar as tarefas a serem adicionadas por parâmetro!"
+      )
+    : console.log(listaDeTarefas);
 }
-console.log(listaDeTarefas);

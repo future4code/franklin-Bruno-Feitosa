4dce46c6b5c751bbe04daa33f31ca80a4d9839c5
“Bruno <bruno.feitosa@grupoboticario.com.br>
2022-08-11 13:36:58 -0300

    Adicionando algumas rotas ao projeto

--- modulo3/intro-express/src/index.ts
+++ modulo3/intro-express/src/index.ts
@@ -1,5 +1,8 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
+
+// Cores
+const green: string = "\x1b[32m";
 
 const app = express();
 
@@ -7,5 +10,47 @@ app.use(express.json());
 app.use(cors());
 
 app.listen(3003, () => {
-  console.log("Server is running in http://localhost:3003");
+  console.log(green + "O servidor está rodando na porta 3003...");
+});
+
+app.get("/", (request: Request, response: Response) => {
+  response.status(200).send({ message: "Acessou a url base com sucesso." });
+});
+
+type User = {
+  id: number;
+  name: string;
+  phone: string;
+  email: string;
+  website: string;
+};
+
+const Bruno: User = {
+  id: 1,
+  name: "Bruno Britto",
+  phone: "123456789",
+  email: "bruno@email.com",
+  website: "www.bruno.com.br",
+};
+
+const baseDeDados: object[] = [
+  Bruno, // Coloquei o usuário criado na linha 28.
+  {
+    id: 2,
+    name: "Thiago Britto",
+    phone: "123123123",
+    email: "thiago@email.com",
+    website: "www.thiago.com.br",
+  },
+  {
+    id: 3,
+    name: "Lucas Britto",
+    phone: "412312321",
+    email: "lucas@email.com",
+    website: "www.lucas.com.br",
+  },
+];
+
+app.get("/users", (request: Request, response: Response) => {
+  response.status(200).send(baseDeDados);
 });

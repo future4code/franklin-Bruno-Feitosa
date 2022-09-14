3b514e8539cd88f40cb38c6ffe6965b7e068f8ac
“Bruno <bruno.feitosa@grupoboticario.com.br>
2022-09-14 01:36:56 -0300

    Finalizando atividade

--- modulo3/arquitetura-de-software-1/src/business/UserBusiness.ts
+++ modulo3/arquitetura-de-software-1/src/business/UserBusiness.ts
@@ -1,55 +1,144 @@
-import UserDatabase from "../database/UserDatabase"
-import { User, USER_ROLES } from "../model/User"
-import { Authenticator, ITokenPayload } from "../services/Authenticator"
-import { HashManager } from "../services/HashManager"
-import { IdGenerator } from "../services/IdGenerator"
+import UserDatabase from "../database/UserDatabase";
+import { User, USER_ROLES } from "../model/User";
+import { Authenticator, ITokenPayload } from "../services/Authenticator";
+import { HashManager } from "../services/HashManager";
+import { IdGenerator } from "../services/IdGenerator";
 
 export default class UserBusiness {
-    public signup = async (input: any) => {
-        const name = input.name
-        const email = input.email
-        const password = input.password
+  public signup = async (input: any) => {
+    const name: string = input.name;
+    const email: string = input.email;
+    const password: string = input.password;
 
-        if (!name || typeof name !== "string") {
-            throw new Error("Parâmetro 'name' inválido")
-        }
-
-        if (!email || typeof email !== "string") {
-            throw new Error("Parâmetro 'email' inválido")
-        }
-
-        if (!email.includes('@')) {
-            throw new Error("Email inválido")
-        }
-
-        const idGenerator = new IdGenerator()
-        const id = idGenerator.generate()
-
-        const hashManager = new HashManager()
-        const hashPassword = await hashManager.hash(password)
-
-        const user = new User(
-            id,
-            name,
-            email,
-            hashPassword
-        )
-
-        const userDatabase = new UserDatabase()
-        await userDatabase.createUser(user)
-
-        const payload: ITokenPayload = {
-            id: user.getId(),
-            role: user.getRole()
-        }
-
-        const authenticator = new Authenticator()
-        const token = authenticator.generateToken(payload)
-
-        const response = {
-            token
-        }
-
-        return response
+    if (!name || typeof name !== "string") {
+      throw new Error("Parâmetro 'name' inválido");
     }
+
+    if (!email || typeof email !== "string") {
+      throw new Error("Parâmetro 'email' inválido");
+    }
+
+    if (!email.includes("@")) {
+      throw new Error("Email inválido");
+    }
+
+    const idGenerator = new IdGenerator();
+    const id = idGenerator.generate();
+
+    const hashManager = new HashManager();
+    const hashPassword = await hashManager.hash(password);
+
+    const user = new User(id, name, email, hashPassword);
+
+    const userDatabase = new UserDatabase();
+    await userDatabase.createUser(user);
+
+    const payload: ITokenPayload = {
+      id: user.getId(),
+      role: user.getRole(),
+    };
+
+    const authenticator = new Authenticator();
+    const token = authenticator.generateToken(payload);
+
+    const response = {
+      token,
+    };
+
+    return response;
+  };
+
+  public login = async (input: any) => {
+    const email: string = input.email;
+    const password: string = input.password;
+
+    if (!email || typeof email !== "string") {
+      throw new Error("Parâmetro 'email' inválido");
+    }
+
+    if (!email.includes("@")) {
+      throw new Error("Email inválido");
+    }
+
+    const userDatabase = new UserDatabase();
+
+    const userDb = await userDatabase.getUserByEmail(email);
+
+    const hashManager = new HashManager();
+    const comparedPassword = await hashManager.compare(
+      password,
+      userDb.password
+    );
+
+    if (!comparedPassword) {
+      throw new Error("Senha inválida");
+    }
+
+    const user = new User(
+      userDb.id,
+      userDb.name,
+      userDb.email,
+      userDb.hashPassword
+    );
+
+    const payload: ITokenPayload = {
+      id: user.getId(),
+      role: user.getRole(),
+    };
+
+    const authenticator = new Authenticator();
+    const token = authenticator.generateToken(payload);
+
+    const response = {
+      token,
+    };
+
+    return response;
+  };
+
+  public getAllUsers = async (token: string): Promise<any> => {
+    const tokenDb: string = token;
+
+    if (!tokenDb) {
+      throw new Error("Acesso não autorizado");
+    }
+
+    const userDatabase = new UserDatabase();
+    const users = await userDatabase.getAllUserFromDb();
+
+    const userList: User[] = users.map((user) => {
+      return user;
+    });
+
+    const response = userList;
+
+    return response;
+  };
+
+  public deleteUser = async (token: string, id: string): Promise<any> => {
+    const tokenDb: string = token;
+    const idDb: string = id;
+
+    if (!idDb) {
+      throw new Error("Id não encontrado");
+    }
+
+    if (!tokenDb) {
+      throw new Error("Acesso não autorizado");
+    }
+
+    const userDatabase = new UserDatabase();
+
+    const searchById = await userDatabase.searchUserById(id);
+
+    if (!searchById) {
+      throw new Error("Usuário não existe");
+    }
+
+    await userDatabase.deleteUserFromDb(id);
+
+    const response = { message: "Usuário apagado com sucesso!" };
+
+    return response;
+  };
 }
--- modulo3/arquitetura-de-software-1/src/database/UserDatabase.ts
+++ modulo3/arquitetura-de-software-1/src/database/UserDatabase.ts
@@ -2,24 +2,56 @@ import { IUserDB, User } from "../model/User";
 import { BaseDatabase } from "./BaseDatabase";
 
 export default class UserDatabase extends BaseDatabase {
-    public static TABLE_USERS = "Arq_Users"
+  public static TABLE_USERS = "Arq_Users";
 
-    public toUserDBModel = (user: User) => {
-        const userDB: IUserDB = {
-            id: user.getId(),
-            name: user.getName(),
-            email: user.getEmail(),
-            password: user.getPassword(),
-            role: user.getRole()
-        }
-        
-        return userDB
-    }
+  public toUserDBModel = (user: User) => {
+    const userDB: IUserDB = {
+      id: user.getId(),
+      name: user.getName(),
+      email: user.getEmail(),
+      password: user.getPassword(),
+      role: user.getRole(),
+    };
 
-    public createUser = async (user: User) => {
-        const userDB = this.toUserDBModel(user)
+    return userDB;
+  };
 
-        await BaseDatabase.connection(UserDatabase.TABLE_USERS)
-            .insert(userDB)
-    }
+  public getUserByEmail = async (email: string) => {
+    const user = await BaseDatabase.connection(UserDatabase.TABLE_USERS)
+      .select("*")
+      .where({
+        email,
+      });
+
+    return user[0];
+  };
+
+  public searchUserById = async (id: string) => {
+    const user = await BaseDatabase.connection(UserDatabase.TABLE_USERS)
+      .select("*")
+      .where({
+        id,
+      });
+
+    return user[0];
+  };
+
+  public createUser = async (user: User) => {
+    const userDB = this.toUserDBModel(user);
+
+    await BaseDatabase.connection(UserDatabase.TABLE_USERS).insert(userDB);
+  };
+
+  public getAllUserFromDb = async () => {
+    const user = await BaseDatabase.connection(UserDatabase.TABLE_USERS).select(
+      "*"
+    );
+    return user;
+  };
+
+  public deleteUserFromDb = async (id: string) => {
+    await BaseDatabase.connection(UserDatabase.TABLE_USERS)
+      .delete()
+      .where({ id });
+  };
 }
